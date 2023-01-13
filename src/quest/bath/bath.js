a83edeeba237ac4622df03e352e99fada573eeed
Jarlyyn <jarlyyn@qq.com>
2023-01-14 00:52:54 +0800

    update

--- src/quest/bath/bath.js
+++ src/quest/bath/bath.js
@@ -90,8 +90,8 @@
         }
         if (App.GetItemByName("蛇床子",true)==null){
             App.Commands([
-                App.NewCommand("to", App.Options.NewWalk("yzyp")),
-                App.NewCommand('do', "buy shechuang zi;i2"),
+                App.NewCommand("to", App.Options.NewWalk("suzhoudp")),
+                App.NewCommand('do', "buy im_226;i2"),
                 App.NewCommand('nobusy'),
                 App.NewCommand('function',App.Quest.Common.Bath.Main),
             ]).Push()
